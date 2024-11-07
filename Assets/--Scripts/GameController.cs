c4e8053263617638a3959abfbaa729a65707102e
zaki97 <zki097@gmail.com>
2024-11-07 14:49:40 +0400

    Update GameController.cs

--- Assets/--Scripts/GameController.cs
+++ Assets/--Scripts/GameController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using TMPro;
 public class GameController : MonoBehaviour
 {
+    //v0
     public Button[] buttons;
     public Button playerVsComputerButton;
     public Button playerVsPlayerButton;
